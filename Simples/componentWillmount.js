82e6f0ed63a7016bb6b774d92f3373ff6fb45677
JaviOvi <106032666+javiovi@users.noreply.github.com>
2022-11-13 16:54:28 -0300

    Create componentWillmount.js
    Usa el método de ciclo de vida componentDidMount
    La mayoría de los desarrolladores web, en algún momento, necesitan llamar al endpoint de un API para obtener datos. Si estás trabajando con React, es importante saber dónde realizar esta acción.
    
    La mejor práctica con React es ubicar las llamadas API o cualquier llamada a tu servidor en el método de ciclo de vida componentDidMount(). Este método se llama después de que un componente es montado (mounted) en el DOM. Cualquier llamada a setState() aquí desencadenará un re-renderizado de tu componente. Cuando se llame a una API en este método, y se modifique el estado con los datos que la API devuelve, automáticamente se ejecutará una actualización una vez que los datos sean recibidos.

---
+++ Simples/componentWillmount.js
@@ -0,0 +1,93 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // Cambia el código debajo de esta línea
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+
+  }
+  componentWillUnmount() {
+document.removeEventListener("keydown",this.handleKeyPress);
+  }
+  // Cambia el código encima de esta línea
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! '
+    }));
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
+
+
+/*Optimiza re-renderizadores con shouldComponentUpdate*/
+
+
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Cambia el código debajo de esta línea
+   if (nextProps.value % 2 == 0)  {
+     return true;
+   }
+
+return false;
+    // Cambia el código encima de esta línea
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
